0f64085a7861547b1dda3dda3f2905844565039b
SUBHADEEP SAMANTA <140041464+Subhadeep-Samanta@users.noreply.github.com>
2025-03-24 14:59:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 import express from "express"
 import connectDB from "./db.js"
 import dotenv from "dotenv"
-
+import router from "./routes.js"
 dotenv.config()
 connectDB()
 const PORT=process.env.PORT||3000
 
 const app=express()
 app.use(express.json())
-
+app.use("/api",router)
 app.listen(PORT,()=>{
     console.log(`server is running on port http://localhost${PORT}`);
 })
